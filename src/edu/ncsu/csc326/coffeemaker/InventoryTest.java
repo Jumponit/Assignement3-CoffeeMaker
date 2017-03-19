54f993f599486997a1fdb1c75c52be97234663f4
laciecochran <laciecochran@yahoo.com>
2017-03-19 15:08:02 -0400

    Added InventoryTest

---
+++ src/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -0,0 +1,115 @@
+/*
+ * InventoryTest - JUnit test cases for the Inventory class of CoffeeMaker
+ * 
+ * @author: Lacie Cochran
+ */
+
+package edu.ncsu.csc326.coffeemaker;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InventoryTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInventory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetChocolate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetChocolate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddChocolate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoffee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoffee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddCoffee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMilk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMilk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMilk() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSugar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetSugar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSugar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnoughIngredients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUseIngredients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
