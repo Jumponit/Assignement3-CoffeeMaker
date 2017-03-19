3350d5ea726e0814b6a9c4b1aa12e1111c33a4e8
Taylor Morris <jumponit1993@gmail.com>
2017-03-19 15:01:19 -0400

    Add files via upload

---
+++ src/edu/ncsu/csc326/coffeemaker/RecipeTest.java
@@ -0,0 +1,112 @@
+package edu.ncsu.csc326.coffeemaker;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RecipeTest
+{
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @Test
+    public void testHashCode()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testRecipe()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAmtChocolate()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAmtChocolate()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAmtCoffee()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAmtCoffee()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAmtMilk()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAmtMilk()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAmtSugar()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetAmtSugar()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetName()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetName()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetPrice()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetPrice()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString()
+    {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEqualsObject()
+    {
+        fail("Not yet implemented");
+    }
+
+}
