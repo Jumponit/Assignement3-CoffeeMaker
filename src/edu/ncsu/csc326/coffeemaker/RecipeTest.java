a46a1b627fb14b41ed0f1d8f83debc8db6db3450
Taylor Morris <jumponit1993@gmail.com>
2017-03-20 13:01:54 -0400

    Add files via upload

--- src/edu/ncsu/csc326/coffeemaker/RecipeTest.java
+++ src/edu/ncsu/csc326/coffeemaker/RecipeTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+
 public class RecipeTest
 {
 
@@ -16,97 +18,225 @@ public class RecipeTest
     @Test
     public void testHashCode()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        recipe.setName("Bobby's Chicken");
+        int x = recipe.hashCode();
+        int y = recipe.hashCode();
+        assertTrue("Bad hash", (x == y && x >= 0));
     }
 
     @Test
     public void testRecipe()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertNotNull("Object should not be null, but is.", recipe);
     }
 
     @Test
     public void testGetAmtChocolate()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Amount not correct, chocolate", recipe.getAmtChocolate(), 0);
     }
 
     @Test
     public void testSetAmtChocolate()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setAmtChocolate("5");
+        }
+        catch (RecipeException e) {
+            fail("RecipeException thrown");
+        }
+        assertEquals("Amount not correct, chocolate", recipe.getAmtChocolate(), 5);
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtChocolateBadNumber() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtChocolate("Bob");
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtChocolateLessThanZero() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtChocolate("-5");
     }
 
     @Test
     public void testGetAmtCoffee()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Amount not correct, coffee", recipe.getAmtCoffee(), 0);
     }
 
     @Test
     public void testSetAmtCoffee()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setAmtCoffee("5");
+        }
+        catch (RecipeException e) {
+            fail("RecipeException thrown");
+        }
+        assertEquals("Amount not correct, coffee", recipe.getAmtCoffee(), 5);
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtCoffeeBadNumber() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtCoffee("Bob");
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtCoffeeLessThanZero() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtCoffee("-5");
     }
 
     @Test
     public void testGetAmtMilk()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Amount not correct, milk", recipe.getAmtMilk(), 0);
     }
 
     @Test
     public void testSetAmtMilk()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setAmtMilk("5");
+        }
+        catch (RecipeException e) {
+            fail("RecipeException thrown");
+        }
+        assertEquals("Amount not correct, milk", recipe.getAmtMilk(), 5);
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtMilkBadNumber() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtMilk("Bob");
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtMilkLessThanZero() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtMilk("-5");
     }
 
     @Test
     public void testGetAmtSugar()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Amount not correct, sugar", recipe.getAmtSugar(), 0);
     }
 
     @Test
     public void testSetAmtSugar()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setAmtSugar("5");
+        }
+        catch (RecipeException e) {
+            fail("RecipeException thrown");
+        }
+        assertEquals("Amount not correct, sugar", recipe.getAmtSugar(), 5);
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtSugarBadNumber() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtSugar("Bob");
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetAmtSugarLessThanZero() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setAmtSugar("-5");
     }
 
     @Test
     public void testGetName()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Name not correct", recipe.getName(), "");
     }
 
     @Test
     public void testSetName()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        recipe.setName("Test Recipe");
+        assertEquals("Name not correct", recipe.getName(), "Test Recipe");
     }
 
     @Test
     public void testGetPrice()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        assertEquals("Amount not correct, price", recipe.getPrice(), 0);
     }
 
     @Test
     public void testSetPrice()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setPrice("20");
+        }
+        catch (RecipeException e) {
+            fail("RecipeException thrown");
+        }
+        assertEquals("Amount not correct, price", recipe.getPrice(), 20);
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetPriceBadNumber() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setPrice("Bob");
+    }
+    
+    @Test (expected = RecipeException.class)
+    public void testSetPriceLessThanZero() throws RecipeException
+    {
+        Recipe recipe = new Recipe();
+        recipe.setPrice("-5");
     }
 
     @Test
     public void testToString()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        recipe.setName("Test Recipe");
+        assertEquals("Name not correct", recipe.toString(), "Test Recipe");
     }
 
     @Test
     public void testEqualsObject()
     {
-        fail("Not yet implemented");
+        Recipe recipe = new Recipe();
+        Recipe dummy_recipe = null;
+        assertFalse("Recipe should be null", recipe.equals(dummy_recipe));
+        Integer x = 5;
+        assertFalse("Object is not a recipe, should be false", recipe.equals(x));
+        dummy_recipe = new Recipe();
+        assertTrue("Recipes should both be initialized to empty values", recipe.equals(dummy_recipe));
+        dummy_recipe = recipe;
+        assertTrue("Recipes should be equal", recipe.equals(dummy_recipe));
+        
     }
-
 }
